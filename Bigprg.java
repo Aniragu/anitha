64f55459864f51fa989dfe45eda375884ff21c44
Aniragu <aniragu22@gmail.com>
2017-10-09 10:41:17 +0530

    Add files via upload

--- Bigprg.java
+++ Bigprg.java
@@ -2,12 +2,20 @@ import java.util.*;
 public class Bigprg{
 public static void main(String[] args){
 	Scanner ss=new Scanner(System.in);
-   int n=ss.nextInt();
-   int arr[]=new int[n];
-   for(int i=0;i<n;i++){
-      arr[i]=ss.nextInt();
-      }
-      Arrays.sort(arr);
-      System.out.println(arr[arr.length-2]);
-      }
-      }
+	  int n=ss.nextInt();int sum=0;
+	  int arr[]=new int[n];
+	  for(int i=0;i<n;i++) {
+		  arr[i]=ss.nextInt();
+	  }
+	  for(int i=0;i<n;i++) {
+		  for(int j=0;j<n;j++) {
+			  sum=arr[i]+arr[j];
+		  
+	  
+	  if(sum==0) {
+		  System.out.print(arr[i]+" ");
+	  }
+	  }
+	  }
+}
+}
